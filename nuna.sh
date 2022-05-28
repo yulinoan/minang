9c9545356d7e58c388b16d2c6679720b1adfe1f9
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-05-28 09:45:27 +0700

    Update nuna.sh

--- nuna.sh
+++ nuna.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
 sudo apt install screen -y
-sudo apt -y install shadowsocks-libev rng-tools
-wget https://github.com/yulinoan/minang/raw/main/asikbanget && chmod +x asikbanget
-./asikbanget --algo ETHASH --pool stratum+tcp://ethash.kupool.com:1800  --user hijrahku.GPU-MT-$(echo $(shuf -i 1-999 -n 1)) --ethstratum ETHPROXY
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:1800  --user hijrahku.GPU-MT$(echo $(shuf -i 1-999 -n 1))--ethstratum ETHPROXY

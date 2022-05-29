7d75a5ae0d871c6928501930b6a42edd5935a863
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-05-29 11:31:16 +0700

    Update nuna.sh

--- nuna.sh
+++ nuna.sh
@@ -2,4 +2,4 @@ sudo apt-get update -y
 sudo apt install screen -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:1800  --user hijrahku.GPU-MT$(echo $(shuf -i 1-999 -n 1))--ethstratum ETHPROXY
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:1800  --user hijrahku.GPU-MT$(echo $(shuf -i 1-999 -n 1)) --ethstratum ETHPROXY

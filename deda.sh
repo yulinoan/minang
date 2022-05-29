776567003e6eebfb499c92999cc8e4ad696bfd23
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-05-29 20:21:14 +0700

    Update deda.sh

--- deda.sh
+++ deda.sh
@@ -1,6 +1,6 @@
-pkill -9 tmate
-wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
-tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar zxvf ngrok-v3-stable-linux-amd64.tgz
 rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/ate.sock new-session -d & disown -a' >/dev/null 2>&1
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}"
+./ngrok authtoken 29WVgSUuC3dCwx81svDA2BksCh5_nbkaL5R9RSnJd6AHZqyB
+nohup ./ngrok tcp 22
